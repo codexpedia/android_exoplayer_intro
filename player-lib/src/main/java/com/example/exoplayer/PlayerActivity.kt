6b0c24a150d9cacbfb4ba378e2f615341bf6f013
codexpedia <codexpedia.com@gmail.com>
2019-11-05 18:04:12 -0800

    Upgraded to Androidx

--- player-lib/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ player-lib/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -17,9 +17,9 @@ package com.example.exoplayer
 
 import android.annotation.SuppressLint
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
+import androidx.appcompat.app.AlertDialog
 import android.view.View
 import com.example.exoplayer.MainActivity.Companion.MEDIA_URI
 
